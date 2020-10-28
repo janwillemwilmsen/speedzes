e12ef24f22396e22a650bb4d80b2208bf0499a52
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-10-28 17:37:34 +0100

    eon

---
+++ _data/sites/GGeon.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: "eon", // optional, falls back to object key
+	description: "Eon websites",
+	options: {
+		frequency: 60 * 23, 
+		// frequency: 1 * 100, 
+		// 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.eon.de/",
+		"https://www.eprimo.de/",
+		"https://e-mobility.ewe.de/",
+		"https://www.e-wie-einfach.de/",
+		"https://www.enviam.de/",
+		"https://www.lew.de/",
+		"https://www.rhenag.de/",
+		"https://www.ele.de/",
+		"https://www.suewag.de/",
+		"https://www.energis.de/",
+		"https://www.eon.com/",
+		"https://www.npower.com/",
+		"https://www.eon.se/",
+		"https://www.eon.dk/",
+		"https://www.eon-italia.com/",
+		"https://www.eon.hu/",
+		"https://ker.elmuemasz.hu/",
+		"https://www.eon.cz/",
+		"https://www.eon.ro/",
+		"https://www.essent.be/",
+		"https://www.innogy.pl/",
+		"https://www.innogy.sk/",
+	]
+};
